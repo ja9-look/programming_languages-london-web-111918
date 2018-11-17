630f18716a060e477eae595395514fb29f31414d
Janine Luk <janineluk95@gmail.com>
2018-11-17 22:45:21 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,5 +3,6 @@ def reformat_languages(languages)
   languages.each do |programme_style, langs|
     langs.each do |lang_name, lang_type|
       if new_hash.include?(lang_name)
-        new_hash[lang_name][:style] 
+        new_hash[lang_name][:style] << programme_style
+        
 end
